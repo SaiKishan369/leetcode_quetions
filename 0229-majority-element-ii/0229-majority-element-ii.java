f24dda052848bf7a14f6bce3df740756d99ed963
Sai Kishan S <142758294+SaiKishan369@users.noreply.github.com>
2026-09-20 11:29:26 +0530

    Time: 4 ms (56.47%), Space: 52.8 MB (29.94%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return new ArrayList<>();
+        if(nums.length == 0){
+            return null;
         }
-        Integer candidate1=null;
+        Integer candidate1 = null;
         Integer candidate2=null;
         int count1=0;
         int count2=0;
+
         for(int num:nums){
             if(candidate1 != null && candidate1 == num){
                 count1++;
             }
-            else if(candidate2 != null && candidate2 == num ){
+            else if(candidate2 != null && candidate2 == num){
                 count2++;
             }
-            else if(count1==0){
+            else if(count1 == 0){
                 candidate1=num;
                 count1=1;
             }
-            else if(count2==0){
+            else if(count2 == 0){
                 candidate2=num;
                 count2=1;
             }
@@ -28,18 +29,29 @@ class Solution {
             }
         }
 
-        count1=0;count2=0;
+        List<Integer> result = new ArrayList<>();
+        count1=0;
+        count2=0;
+
         for(int num:nums){
-            if(candidate1 != null && candidate1 == num)count1++;
-            else if(candidate2 != null && candidate2 == num)count2++;
+            if(candidate1 != null && candidate1 == num){
+                count1++;
+            }
+            if(candidate2 != null && candidate2 == num){
+                count2++;
+            }
         }
 
-        List<Integer> result=new ArrayList<>();
         int threshold=nums.length/3;
 
-        if(candidate1 != null && count1 > threshold)result.add(candidate1);
-        if(candidate2 != null && count2 > threshold){
-            if(candidate2 != candidate1)result.add(candidate2);
+        if(candidate1 != null && count1 > threshold){
+            result.add(candidate1);
+        }
+
+        if(candidate2 != null && candidate2 != candidate1){
+            if(count2 > threshold){
+                result.add(candidate2);
+            }
         }
 
         return result;
