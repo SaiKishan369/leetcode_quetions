dca051408c2bc2a4d1bbc02e5f841a4d201c9192
Sai Kishan S <142758294+SaiKishan369@users.noreply.github.com>
2026-09-21 16:19:26 +0530

    Time: 577 ms (82.52%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,4 +1,4 @@
-select c.customer_id
-from customer c
-group by c.customer_id
-having count(distinct(c.product_key)) = (select count(distinct(product_key)) from product);
+select customer_id 
+from customer 
+group by customer_id
+having count(distinct(product_key))=(select count(*) from product); 
