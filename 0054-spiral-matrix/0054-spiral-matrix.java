59b08ecc98a8f225f531f5ac90883557ab8e3e12
Sai Kishan S <142758294+SaiKishan369@users.noreply.github.com>
2026-09-23 07:58:03 +0530

    Time: 0 ms (100%), Space: 43 MB (62.49%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,37 +1,41 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result=new ArrayList<>();
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return result;
         }
-        int row=matrix.length;
-        int col=matrix[0].length;
 
-        int left=0;
-        int right=col-1;
-        int top=0;
-        int bottom=row-1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int top = 0;
+        int bottom = matrix.length - 1;
 
-        while(left <= right && top <= bottom){
-            for(int i=left;i<=right;i++){
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
             top++;
-            for(int i=top;i<=bottom;i++){
+
+            for (int i = top; i <= bottom; i++) {
                 result.add(matrix[i][right]);
             }
             right--;
-            if(top<=bottom){
-            for(int i=right;i>=left;i--){
-                result.add(matrix[bottom][i]);
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
             }
-            bottom--;}
-            if(left<=right){
-            for(int i=bottom;i>=top;i--){
-                result.add(matrix[i][left]);
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
             }
-            left++;}
         }
+
         return result;
     }
 }
