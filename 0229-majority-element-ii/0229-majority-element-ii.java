282ef2d6df633d84f3705e13d6a5bdf8fa77d6a8
Sai Kishan S <142758294+SaiKishan369@users.noreply.github.com>
2026-09-23 08:07:17 +0530

    Time: 4 ms (56.36%), Space: 52.3 MB (79.24%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        if(nums.length == 0){
+        if(nums.length == 0 ){
             return null;
         }
-        Integer candidate1 = null;
+        Integer candidate1=null;
         Integer candidate2=null;
         int count1=0;
         int count2=0;
 
         for(int num:nums){
-            if(candidate1 != null && candidate1 == num){
-                count1++;
-            }
-            else if(candidate2 != null && candidate2 == num){
-                count2++;
-            }
-            else if(count1 == 0){
+            if(candidate1 != null && candidate1 == num)count1++;
+            else if(candidate2 != null && candidate2 == num)count2++;
+            else if(count1 == 0 ){
                 candidate1=num;
                 count1=1;
             }
@@ -29,32 +25,27 @@ class Solution {
             }
         }
 
-        List<Integer> result = new ArrayList<>();
         count1=0;
         count2=0;
-
         for(int num:nums){
             if(candidate1 != null && candidate1 == num){
                 count1++;
-            }
-            if(candidate2 != null && candidate2 == num){
+        }
+            else if(candidate2 != null && candidate2 == num){
                 count2++;
             }
         }
 
+        List<Integer> result=new ArrayList<>();
         int threshold=nums.length/3;
 
         if(candidate1 != null && count1 > threshold){
             result.add(candidate1);
         }
-
-        if(candidate2 != null && candidate2 != candidate1){
-            if(count2 > threshold){
-                result.add(candidate2);
-            }
+        if(candidate2 != null && count2 > threshold && candidate2 != candidate1){
+           result.add(candidate2);
         }
 
         return result;
-
     }
 }
