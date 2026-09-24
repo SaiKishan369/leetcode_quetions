fa8738eb813c9bb98a6a1faa640054f3591e731b
Sai Kishan S <142758294+SaiKishan369@users.noreply.github.com>
2026-09-24 07:25:30 +0530

    Time: 616 ms (90%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
 select round(
-    sum(case when d.order_date = d.customer_pref_delivery_date  then 1 end )/(select count(distinct customer_id) from delivery) * 100
+    sum(case when d.order_date = d.customer_pref_delivery_date then 1 else 0 end)/(select count(distinct customer_id) from delivery) * 100
     ,2) as immediate_percentage 
-    from delivery d
-    join (select customer_id,min(order_date) as first_orderdate from delivery group by customer_id)x
-    on d.customer_id = x.customer_id and d.order_date = x.first_orderdate;
+from delivery d
+join ( 
+    select delivery_id, customer_id, min(order_date) as first_order
+    from delivery
+    group by customer_id
+) x
+on x.customer_id = d.customer_id and x.first_order = d.order_date;
